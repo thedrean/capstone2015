53ae19293bdf135f7ab22d26edadd0c626da3164
newmediacenter <newmediacenter@nmc02.american.edu>
2015-09-30 13:59:54 -0400

    adding unity demo

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var speed = 1.0f;
+		var movekey = new Vector3 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0);
+		transform.position += movekey * speed * Time.deltaTime;
+	
+	}
+}
